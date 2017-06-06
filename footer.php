e09601997cc2dfae7396d464ba2d9750830eec0e
fabien-raymondaud <fabien.raymondaud@gmail.com>
2017-06-06 12:06:54 +0200

    gestion globale des boutons de partage

--- footer.php
+++ footer.php
@@ -99,6 +99,22 @@ if(is_home() || is_front_page()){
 			<ul class="nav-mentions unstyled">
 				<li class="entree-secondaire typo1"><a href="<?php the_permalink(99);?>">Mentions légales</a></li>
 			</ul>
+
+			<div class="nav-partager">
+				<h4 class="size12 typo1"><span>Partager</span></h4>
+				<ul class="unstyled">
+					<li>
+						<a href="https://twitter.com/share?text=Mémoires électriques, le webzine <?php echo get_permalink();?>" class="twitter" target="_blank">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36.02 36.02"><g id="Calque_1-2" data-name="Calque 1"><path class="cls-1" d="M18,36A18,18,0,1,0,0,18,18,18,0,0,0,18,36"/><path class="cls-2" d="M27,13.29a7.46,7.46,0,0,1-2.12.58,3.7,3.7,0,0,0,1.63-2,7.35,7.35,0,0,1-2.35.9,3.7,3.7,0,0,0-6.4,2.53,3.6,3.6,0,0,0,.1.84,10.49,10.49,0,0,1-7.63-3.87,3.71,3.71,0,0,0,1.14,4.94,3.72,3.72,0,0,1-1.68-.46v0a3.7,3.7,0,0,0,3,3.63,3.69,3.69,0,0,1-1.67.06A3.7,3.7,0,0,0,14.48,23a7.44,7.44,0,0,1-4.6,1.58A7.32,7.32,0,0,1,9,24.55a10.52,10.52,0,0,0,16.2-8.87c0-.16,0-.32,0-.48A7.46,7.46,0,0,0,27,13.29"/></g></svg>
+						</a>
+					</li>
+					<li>
+						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink();?>" class="facebook" target="_blank">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.02 34.02"><g id="Calque_1-2" data-name="Calque 1"><path class="cls-1" d="M17,34A17,17,0,1,0,0,17,17,17,0,0,0,17,34"/><path class="cls-2" d="M12.9,14.13h1.76V12.42a4.57,4.57,0,0,1,.57-2.64A3.13,3.13,0,0,1,18,8.5a11.05,11.05,0,0,1,3.15.32l-.44,2.6a6,6,0,0,0-1.42-.21c-.68,0-1.3.24-1.3.93v2h2.8l-.2,2.54H18v8.84h-3.3V16.67H12.9Z"/></g></svg>
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 
 		<div class="back-to-top">
