edb4e35ac4b0db766b5e60d07fd2446fb069cebc
fabien-raymondaud <fabien.raymondaud@gmail.com>
2017-06-06 10:26:54 +0200

    gestion responsive js slider retro

--- src/js/general.js
+++ src/js/general.js
@@ -31,22 +31,44 @@ $(document).ready(function() {
 	});
 
 	var compteur_click_nav_retro = 0;
+	var winwidth = document.body.clientWidth;
+	var multiplicateur = 85;
+	var decalage = 0;
+	var largeur_nav = 0;
+	
+	if(winwidth<=620){
+		largeur_nav = winwidth - 60;
+		multiplicateur = largeur_nav;
+		$('.flexslider-retro .flex-control-paging li').css('width', largeur_nav+'px');
+	}
 
 	$('.flexslider-retro .flex-next').click(function(){
 		compteur_click_nav_retro--;
-		var decalage = compteur_click_nav_retro * 85;
+		decalage = compteur_click_nav_retro * multiplicateur;
 		$('.flex-control-nav').css('left', decalage+'px');
 
 	});
 
 	$('.flexslider-retro .flex-prev').click(function(){
 		compteur_click_nav_retro++;
-		var decalage = compteur_click_nav_retro * 85;
+		decalage = compteur_click_nav_retro * multiplicateur;
 		$('.flex-control-nav').css('left', decalage+'px');
-
 	});
+	
 
 	$(window).resize( function() {
-        
+		winwidth = document.body.clientWidth;
+		if(winwidth<=620){
+			largeur_nav = winwidth - 60;
+			$('.flexslider-retro .flex-control-paging li').css('width', largeur_nav+'px');
+			multiplicateur = largeur_nav;
+		}
+		else{
+			$('.flexslider-retro .flex-control-paging li').css('width', '61px');
+			multiplicateur = 85;
+		}
+
+		decalage = compteur_click_nav_retro * multiplicateur;
+		$('.flex-control-nav').css('left', decalage+'px');
     });
 });
